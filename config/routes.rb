c87254e66172e81efa3256ab32368e9fcd4705e5
John Leavell <johnleavell@gmail.com>
2023-05-25 20:44:36 +0000

    completed helper methods 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,23 @@ Rails.application.routes.draw do
   root 'movies#index'
 
   # Routes for the Movie resource:
-
+  resources :movies
   # CREATE
-  post '/movies' => 'movies#create', as: :movies # movies_url and movies_path
-  get '/movies/new' => 'movies#new', as: :new_movie # new_movie_url and new_movie_path
+  # post '/movies' => 'movies#create', as: :movies # movies_url and movies_path
+  # get '/movies/new' => 'movies#new', as: :new_movie # new_movie_url and new_movie_path
 
   # READ
-  get '/movies' => 'movies#index'
+  # get '/movies' => 'movies#index'
 
   # example of changing the name of the route
-  get '/movies/:id' => 'movies#show', as: :movie # movie_path() (expects an argument to populate :id segment)
+  # get '/movies/:id' => 'movies#show', as: :movie # movie_path() (expects an argument to populate :id segment)
 
   # UPDATE
-  patch '/movies/:id' => 'movies#update'
-  get '/movies/:id/edit' => 'movies#edit', as: :edit_movie # edit_movie_path() (expects an argument to populate :id segment)
+  # patch '/movies/:id' => 'movies#update'
+  # get '/movies/:id/edit' => 'movies#edit', as: :edit_movie # edit_movie_path() (expects an argument to populate :id segment)
 
   # DELETE
-  delete '/movies/:id' => 'movies#destroy'
+  # delete '/movies/:id' => 'movies#destroy'
 
   #------------------------------
 end
